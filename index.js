7baa897fdb160498f8ecd81f76e91daf6632620a
shashank singh bhadauria <codingtime1994@gmail.com>
2024-08-15 11:28:48 +0530

    NODE JS first basic program

---
+++ index.js
@@ -0,0 +1,10 @@
+const a=require("./math")
+const s=require("./math")
+const d=require("./math")
+const m=require("./math")
+
+console.log(a.add(5,5));
+console.log(s.sub(5,5));
+console.log(d.div(5,5));
+console.log(m.mul(5,5));
+
